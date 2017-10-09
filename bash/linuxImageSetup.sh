e17781d16ea5d1b50941946636e0f08933de4713
Reinold Rojas <rojasreinold@gmail.com>
2017-10-09 13:29:30 -0400

    Update linuxImageSetup.sh

--- bash/linuxImageSetup.sh
+++ bash/linuxImageSetup.sh
@@ -6,12 +6,12 @@ echo "What is the new mac Address?\n"
 read newMacAddress
 #reconfigure networking settings
 if -f /etc/network
-    sed -ir 's/host-\d\d\d-name/host-$newHostName-name/g' /etc/network
+    sed -ir 's/host-[0-9][0-9][0-9]-name/host-$newHostName-name/g' /etc/network
 fi
 
 for item in $(ls /etc/sysconfig/network-scripts/ifcfg-e*)
 do
-    sed -ir 's/host-\d\d\d-name/$newHostName/g' $item
+    sed -ir 's/host-[0-9][0-9][0-9]-name/$newHostName/g' $item
     sed -r 's/HWADDR=.*/HWADDR=$newMacAddress/g' $item
 done
 
@@ -23,7 +23,7 @@ echo "configureed networking"
 
 #bind to domain
 echo "binding to domain"
-sed -r 's/ldap_sasl_authid = HOST-\d\d\d-NAME\$@HOST.COMPANY.LOCAL/ldap_sasl_authid = HOST-$newHostName-NAME\$@HOST.COMPANY.COM/g' -i /etc/sssd/sssd.conf 
+sed -r 's/ldap_sasl_authid = HOST-[0-9][0-9][0-9]-NAME\$@HOST.COMPANY.LOCAL/ldap_sasl_authid = HOST-$newHostName-NAME\$@HOST.COMPANY.COM/g' -i /etc/sssd/sssd.conf 
 net ads join -U username
 
 service sssd restart 

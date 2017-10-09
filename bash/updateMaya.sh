abf053f41319eeccdfedeed004cfb2bf0a1dabbf
Reinold Rojas <rojasreinold@gmail.com>
2017-10-09 12:08:37 -0400

    Create updateMaya.sh

---
+++ bash/updateMaya.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Change USERS maya preferences as sometimes the shared dir causes issues
+
+
+for item in $(ls /USERS/*/maya/20*/prefs/synColorConfig.xml); do
+
+    sed -r 's/<SharedHome dir="\/USERS\/genericUser\/maya\/synColor\/Shared\/" \/>/<SharedHome dir="\/tmp\/" \\> /g' -i $item
+    if [ $? ]
+        then
+        echo "Checked " $item
+    fi
+    done
+    

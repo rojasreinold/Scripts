8651bd2ea6fdc8e839c3e61c806a27c81e456faa
Reinold Rojas <rojasreinold@gmail.com>
2017-10-09 12:18:11 -0400

    Create testTemp.sh

---
+++ bash/testTemp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while [ 0 == 0 ]
+do
+	echo `date` > /root/temperaturesTest
+	for thing in `cat /root/portlist`;
+	do
+        	temp_str=`/usr/sbin/tw_cli $thing show temperature | xargs`;
+        	celc=`echo $temp_str | cut -d' ' -f 4`;
+        	temp_f=`echo $celc*9/5+32 | bc`;
+        	both_temps=`echo "$temp_str ($temp_f in F)"`;
+        	echo $both_temps >> /root/temperaturesTest;
+	done;
+	cat /root/temperaturesTest
+	sleep 60s
+done;

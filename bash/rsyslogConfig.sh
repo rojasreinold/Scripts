bb8ca942e9e4c568e12aa192e2763244886e28d1
Reinold Rojas <rojasreinold@gmail.com>
2017-10-10 14:28:33 -0400

    Update rsyslogConfig.sh

--- bash/rsyslogConfig.sh
+++ bash/rsyslogConfig.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+###Config rsyslog to connect to central log server
 #replaces *.* @IPADDRESS with graylog ip address, port, and network protocol
 sed -r 's/^#?\*\.\*\s@{1,2}([0-9]{1,3}\.){3}[0-9]{1,3}(:[0-9]+)?/\*\.\* @@192\.168\.1\.1:514/g' -i /etc/rsyslog.conf
 

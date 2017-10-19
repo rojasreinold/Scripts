338348cdcdb6ca430d38ff55c3db5c6b900c9fc2
Reinold Rojas <rojasreinold@gmail.com>
2017-10-19 16:04:11 -0400

    Update makeDHCP.py
    changed how dhcp was configed

--- python/makeDHCP.py
+++ python/makeDHCP.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-### make file to configure Active Directory dhcp server
 import MySQLdb
 import sys
 
@@ -8,28 +7,28 @@ if __name__ == "__main__":
 
 
     #Connect to database
-    db = MySQLdb.connect(host="host",user="user",passwd="passwd",db="db")
+    db = MySQLdb.connect(host="nycs001",user="root",passwd="nycs001",db="dydhcp")
     cursor =db.cursor()
     dhcpFile = open('dhcpConf.txt','w')
 
 
-    cursor.execute("""select ip, netmask, id, name, range_lower, range_upper from subnets""")
+    cursor.execute("""select id, netmask, name from subnets""")
 
     for rows in cursor:
 #Make dhcp scopes
         currentRow = cursor.fetchone()
         line = ""
-        line += "dhcp server dhcp_server add scope "
+        line += "dhcp server nycdc add scope "
         line += str(currentRow[0]) + " "
         line += str(currentRow[1]) + " "
-        line += str(currentRow[2]) + " " + str(currentRow[3]) + "\n"
+        line += str(currentRow[2]) + " " + str(currentRow[2]) + "\n"
         dhcpFile.write(line)
 
 
 #Set state for scopes on
         line = ""
-        line += "dhcp server dhcp_server scope "
-        line += str(currentRow[2]) + " set state 0 \n"
+        line += "dhcp server nycdc scope "
+        line += str(currentRow[0]) + " set state 0 \n"
         dhcpFile.write(line)
 
 #Define ip address pools
@@ -37,7 +36,7 @@ if __name__ == "__main__":
     for rows in cursor:
         currentRow = cursor.fetchone()
         line = ""
-        line += "dhcp server dhcp_server scope "
+        line += "dhcp server nycdc scope "
         line += str(currentRow[2]) + " add iprange "
         line += str(currentRow[4]) + " "
         line += str(currentRow[5]) + "\n"
@@ -51,27 +50,28 @@ if __name__ == "__main__":
         line = ""
         line += "dhcp server "
         line += str(currentRow[0])
-        line += "scope "
+        line += " scope "
         line += str(currentRow[1])
         line += " add reservedip "
         line += str(currentRow[2]) + " "
         line += str(currentRow[3]) + " "
-        line += str(currentRow[0]) + " "
-        line += "DHCP \n"
+        line += str(currentRow[0]) + " \""
+        line += str(currentRow[0]) + "\" "
+        line += "BOTH \n"
         dhcpFile.write(line)
 
 
-#Set state for scopes on
+Set state for scopes on
     cursor.execute("""select ip, netmask, id, name, range_lower, range_upper from subnets""")
     for rows in cursor:
         currentRow = cursor.fetchone()
         line = ""
-        line += "dhcp server dhcp_server scope "
+        line += "dhcp server nycdc scope "
         line += str(currentRow[2]) + " set state 1 \n"
         dhcpFile.write(line)
 
 
-#
     cursor.close()
     db.close()
 #now just do "netsh exec c:\path\to\dhcpConf.txt
+#also make sure to set default gateway and dns servers for all scopes (server wide)
